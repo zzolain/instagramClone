8936ad90625509e935aef3e7b178b5431124099c
Jinsol Kim <jskim@gdcomm.io>
2019-05-10 08:57:28 +0900

    #1.2 Setting Up the Server

--- src/server.js
+++ src/server.js
@@ -1,20 +1,11 @@
 require('dotenv').config();
 import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-  type Query {
-    hello: String!
-  }
-`;
+const server = new GraphQLServer({ schema });
+server.express.use(logger('dev'))
 
-const resolvers = {
-  Query: {
-    hello: () => 'hi',
-  },
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
-
-server.start({ port: PORT }, () => console.log(`Server running on http://localhost:${PORT}`));
+server.start({ port: PORT }, () => console.log(`🙏  Server running on http://localhost:${PORT}`));
