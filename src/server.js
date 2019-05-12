7ebc783ffc994cbd1cb5bade46f366ca26a9e874
zzolain <zzolain@gmail.com>
2019-05-12 19:32:23 +0900

    #3.4 Passport JWT part One

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
@@ -6,6 +8,6 @@ import schema from './schema';
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
-server.express.use(logger('dev'))
+server.express.use(logger('dev'));
 
 server.start({ port: PORT }, () => console.log(`🙏  Server running on http://localhost:${PORT}`));
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,31 @@
-import { adjectives, nouns } from "./words";
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
-  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`
-}
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = email => {
+  const options = {
+    auth: {
+      api_key: process.env.SENDGRID_KEY,
+    },
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: 'zzolain@instagramClone.com',
+    to: address,
+    subject: 'Login Secret for InstagramClone 🔐',
+    html: `Hello! Your login secret is<br/><strong>${secret}</strong><br/>Copy and paste on the app/website to log in`,
+  };
+  return sendMail(email);
+};
