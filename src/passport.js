d03b7b2d5f3ca0f9a89404fb2daba10fa8f2c2f7
Jinsol Kim <jskim@gdcomm.io>
2019-05-13 00:24:11 +0900

    #3.6 Passport JWT part Three

--- src/passport.js
+++ src/passport.js
@@ -1,17 +1,32 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
-
 import passport from 'passport';
-import JwtStrategy from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { prisma } from '../generated/prisma-client';
 
 const jwtOptions = {
-  jwtFromRequest: JwtStrategy.ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secret: process.env.JWT_SECRET
-}
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
 
-const verifyUser = (payload, done) => {
+const verifyUser = async (payload, done) => {
+  try {
+    const user = await prisma.user({ id: payload.id });
+    if (user !== null) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    return done(error, false);
+  }
+};
 
-}
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate('jwt', { sessions: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
 
-passport.use(new JwtStrategy(jwtOptions, verifyUser));
+passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,7 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
 import { adjectives, nouns } from './words';
 import nodemailer from 'nodemailer';
 import sgTransport from 'nodemailer-sendgrid-transport';
+import jwt from 'jsonwebtoken';
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -29,3 +27,5 @@ export const sendSecretMail = (address, secret) => {
   };
   return sendMail(email);
 };
+
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
